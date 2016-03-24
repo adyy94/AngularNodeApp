6f43b14ee7416db41fe18d316ee6990d62c5cc96
adyy94 <ady_drozman2007@yahoo.com>
2016-03-24 02:43:30 -0700

    v2

---
+++ mysqltest/view/script.js
@@ -0,0 +1,58 @@
+﻿var app = angular.module('myApp', []);
+app.controller('customersCtrl', function ($scope, $http) {
+    $scope.myData = {};
+    
+    $http.get("/books").then(function (response) {
+        
+        $scope.myData = response.data.Books;
+
+    });
+    
+    $scope.Delete = function (book) {
+        
+        
+        if (confirm('Are you sure you want to delete this?')) {
+            // TODO:  Do something here if the answer is "Ok".
+            
+            $http.delete('books/' + book);
+            // alert('datele au fost sterse cu succes');
+            var index = $scope.myData.indexOf(book);
+            $scope.myData.splice(index, 1);
+
+        }
+
+    };
+    
+    
+    $scope.Save = function () {
+        
+        var data = {
+            bookname: $scope.bookname,
+            authorname: $scope.authorname,
+            price: $scope.price
+        };
+        
+        
+        
+        $http.post('/books', data).success(function (data) {
+            
+            alert('datele au fost salvate cu succes!');
+            $scope.myData.push($scope.data);
+
+        })
+
+    };
+
+
+
+
+
+});
+
+
+app.controller('createCtrl', function ($scope, $http) {
+
+
+
+});
+     
